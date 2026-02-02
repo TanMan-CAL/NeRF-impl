70fde080069006ffcf934c7f626f5722190f6fa2
Tanmay Shah <tanmayshah2007@gmail.com>
2026-02-01 19:19:37 -0500

    fix: cleaned repo and fixed dataloader

--- ray.py
+++
@@ -1,9 +0,0 @@
-from .vec3 import Vec3
-
-class Ray:
-    def __init__(self, origin, direction):
-        self.origin = origin
-        self.direction = direction
-    
-    def at(self, t):
-        return self.origin + t * self.direction
