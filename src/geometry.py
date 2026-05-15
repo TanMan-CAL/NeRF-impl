7af116837038dabdf2ccf84d17912628325f95d8
Tanmay Shah <tanmayshah2007@gmail.com>
2026-05-14 20:04:37 -0400

    commenting for geometry conversions

--- src/geometry.py
+++ src/geometry.py
@@ -1,35 +1,33 @@
-# src/geometry.py
+#src/geometry.py
 import numpy as np
+
 
 class Geometry:
     @staticmethod
     def transform(camera2world, points):
-        ones = np.ones((*points.shape[:-1], 1))
+        ones = np.ones((*points.shape[:-1], 1), dtype=np.float32)
         points_1s = np.concatenate([points, ones], axis=-1)
-
         points_rotat_trans = points_1s @ camera2world.T
-
         return points_rotat_trans[..., :3]
 
     @staticmethod
-    def pixel_to_camera(K, uv, depth=1):
-        fx, fy, cx, cy = K[0,0], K[1,1], K[0,2], K[1,2]
+    def pixel_to_camera(K, uv, depth=1.0):
+        fx, fy = K[0, 0], K[1, 1]
+        cx, cy = K[0, 2], K[1, 2]
 
-        x = (uv[...,0] - cx) / fx * depth
-        y = (uv[...,1] - cy) / fy * depth
+        x = (uv[..., 0] - cx) / fx * depth
+        y = (uv[..., 1] - cy) / fy * depth
         z = np.ones_like(x) * depth
 
-        return np.stack([x, y, z], axis=-1)
-    
+        return np.stack([x, y, z], axis=-1).astype(np.float32)
+
     @staticmethod
     def pixel_to_ray(K, camera2world, uv):
         ray_origin = camera2world[:3, 3]
+        points_camera = Geometry.pixel_to_camera(K, uv, depth=1.0)
+        points_world = Geometry.transform(camera2world, points_camera)
 
-        point = Geometry.pixel_to_camera(K, uv, depth=1.0)
-
-        x = Geometry.transform(camera2world, point)
-
-        ray_direction = x - ray_origin
+        ray_direction = points_world - ray_origin
         ray_direction = ray_direction / np.linalg.norm(ray_direction, axis=-1, keepdims=True)
 
-        return ray_origin, ray_direction
+        return ray_origin.astype(np.float32), ray_direction.astype(np.float32)
